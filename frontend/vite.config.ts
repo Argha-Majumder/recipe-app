57513debcdbbbcbd073f233135cb030749a0bb18
Argha-Majumder <majumderargha847@gmail.com>
2024-03-04 13:50:27 +0530

    minor changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/frontend/', // Base path for your project
+ build: {
+    outDir: 'dist', // Output directory for the build
+ },
   plugins: [react()],
 })
